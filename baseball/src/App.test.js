1a766b3e7bce9ec38d63248ae0d617efe8f3c384
John O'Keefe <jokeefe@weegolobal.com>
2019-06-22 14:50:09 -0400

    added title to Dashboard

--- baseball/src/App.test.js
+++ baseball/src/App.test.js
@@ -2,14 +2,22 @@ import { render } from '@testing-library/react'
 import '@testing-library/react/cleanup-after-each'
 import 'jest-dom/extend-expect'
 import React from 'react'
+import Dashboard from './components/Dashboard/Dashboard'
+import { CountContext } from './contexts/CountContext'
 import App from './App'
 import Display from './components/Display/Display';
-import Dashboard from './components/Dashboard/Dashboard'
 
 
 it('renders without crashing', () => {
     render(<App />)
-    render(<Display />)
-    render(<Dashboard />)
+    render(
+        <CountContext.Provider value={{}}>
+            <Dashboard />
+            <Display />
+        </CountContext.Provider>
+    )
 })
 
+it('displays buttons on screen', () => {
+    
+})
--- baseball/src/components/Dashboard/Dashboard.js
+++ baseball/src/components/Dashboard/Dashboard.js
@@ -4,6 +4,7 @@ import Display from '../Display/Display'
 export default function Dashboard() {
     return (
         <>
+            <h1>Dashboard</h1>
             <Display />
         </>
     )
